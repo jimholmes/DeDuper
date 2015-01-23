fe80187c2d4c0e6bcd5c7b0aaf01868aaf179fe3
Jim Holmes <jim@iterativerose.com>
2015-01-23 08:38:29 -0500

    Added Exe

---
+++ DeDuper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+using Logic;
+
+
+namespace DeDuper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                DirectoryWalker walker = new DirectoryWalker(options.Directory);
+                walker.ProcessDirectoryTree();
+            }
+        }
+    }
+
+    class Options
+    {
+        [Option('d', "Directory", Required = true,
+            HelpText = "Directory to start (top level)")]
+        public string Directory { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+        
+    }
+}
